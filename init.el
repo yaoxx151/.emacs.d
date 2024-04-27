7b4a8948093b6ecf19fb8bf98198495c01825cb5
yaoxx151 <yysminn@gmail.com>
2024-04-27 13:43:09 -0700

    Hopefully make GUI faster.

--- init.el
+++ init.el
@@ -1,6 +1,58 @@
+;;; init.el --- -*- lexical-binding: t -*-
+
 ;; Reduce the frequency of garbage collection by making it happen on
-;; each 200MB of allocated data.
-(setq gc-cons-threshold 200000000)
+;; each 100MB of allocated data.
+(setq gc-cons-threshold 100000000)
+
+(setq package-enable-at-startup nil)
+
+(defvar file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(setq site-run-file nil)
+
+(defvar better-gc-cons-threshold 134217728 ; 128mb
+  "The default value to use for `gc-cons-threshold'.
+
+If you experience freezing, decrease this.  If you experience stuttering, increase this.")
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold better-gc-cons-threshold)
+            (setq file-name-handler-alist file-name-handler-alist-original)
+            (makunbound 'file-name-handler-alist-original)))
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (if (boundp 'after-focus-change-function)
+                (add-function :after after-focus-change-function
+                              (lambda ()
+                                (unless (frame-focus-state)
+                                  (garbage-collect))))
+              (add-hook 'after-focus-change-function 'garbage-collect))
+            (defun gc-minibuffer-setup-hook ()
+              (setq gc-cons-threshold (* better-gc-cons-threshold 2)))
+
+            (defun gc-minibuffer-exit-hook ()
+              (garbage-collect)
+              (setq gc-cons-threshold better-gc-cons-threshold))
+
+            (add-hook 'minibuffer-setup-hook #'gc-minibuffer-setup-hook)
+            (add-hook 'minibuffer-exit-hook #'gc-minibuffer-exit-hook)))
+
+;;System configs.
+(defconst *sys/linux*
+  (eq system-type 'gnu/linux)
+  "Are we running on a GNU/Linux system?")
+
+(defconst *sys/mac*
+  (eq system-type 'darwin)
+  "Are we running on a Mac system?")
+
+;; Map Mac's command key to Meta only in GUI. Do it in iTerm2 config.
+(if *sys/mac*
+    (setq mac-command-modifier 'meta
+	  mac-right-command-modifier 'super))
 
 (require 'package)
 (setq package-enable-at-startup nil)
